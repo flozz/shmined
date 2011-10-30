307a08e7fb5d8c3df5249e94a48f21c46732f53a
Fabien LOISON <flo@flogisoft.com>
2011-10-30 14:02:45 +0100

    * Grid generation implemented * Grid refresh (draw) implemented

--- src/shmined.sh
+++ src/shmined.sh
@@ -37,6 +37,20 @@ cd "${0%/*}" 1> /dev/null 2> /dev/null
 . kbmouse.sh
 
 
+COLOR_WIN_BG=237
+COLOR_CELL=(253 255)
+COLOR_FLAG_BG=(160 196)
+COLOR_FLAG_FG=255
+COLOR_WATER=(250 250)
+COLOR_NUMB=(21 28 196 18 90 124 52 232)
+
+
+NUMB_MINES=25
+GRID_SIZE=24
+GRID=()
+DISP_GRID=()
+
+
 copyright_screen() {
 	## Display the copyright screen.
 
@@ -57,8 +71,79 @@ copyright_screen() {
 }
 
 
+grid_init() {
+	## Generates a grid with random mines.
+
+	#Initialize grids
+	for ((i=0 ; i<$(($GRID_SIZE**2)) ; i++)) ; do
+		GRID[$i]="."
+		DISP_GRID[$i]="."
+	done
+
+	#Put the mines
+	mines=$NUMB_MINES
+	while [ $mines -gt 0 ] ; do
+		randpos=$(($RANDOM*$GRID_SIZE**2/32767))
+		if [ ${GRID[$randpos]} == "." ] ; then
+			GRID[$randpos]="O"
+			mines=$(($mines-1))
+		fi
+	done
+}
+
+
+grid_refresh() {
+	i=0
+	x=1
+	y=1
+	c=0
+	while [ $y -le $GRID_SIZE ] ; do
+		#Go to the right line
+		tui_cursor_move_to 1 $y
+		while [ $x -le $GRID_SIZE ] ; do
+			#Set the color variant
+			test $((($i+$y)%2)) == 0 && c=1 || c=0
+			#Draw a cell
+			case ${DISP_GRID[$i]} in
+				.) #Cell
+					tui_color_set_background ${COLOR_CELL[$c]}
+					test ${GRID[$i]} == O && tui_color_set_background 213 #FIXME
+					echo -n "  "
+					;;
+				f) #Flag
+					tui_color_set_background ${COLOR_FLAG_BG[$c]}
+					tui_color_set_foreground ${COLOR_FLAG_FG}
+					echo -n '!!'
+					;;
+				w) #Water
+					tui_color_set_background ${COLOR_WATER[$c]}
+					echo -n "  "
+					;;
+				1|2|3|4|5|6|7|8) #Number
+					tui_color_set_background ${COLOR_WATER[$c]}
+					tui_color_set_foreground ${COLOR_NUMB[$((${DISP_GRID[$i]}-1))]}
+					echo -n "${DISP_GRID[$i]} "
+					;;
+			esac
+			#Increment
+			i=$(($i+1))
+			x=$(($x+1))
+		done
+		#Increment
+		x=1
+		y=$(($y+1))
+	done
+}
+
+
 #Main
 kbmouse_terminal_init
 tui_window_set_title "${APPNAME}"
 copyright_screen
+
+tui_draw_rect $COLOR_WIN_BG 1 1 $(tui_window_get_width) $(tui_window_get_height)
+grid_init
+grid_refresh
+read #FIXME
+
 kbmouse_terminal_release
